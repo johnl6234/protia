2dc2a626e0357a328e6306d5fc15cff79609d40b
John Lockham <47853652+johnl6234@users.noreply.github.com>
2022-07-01 17:17:09 +0100

    bug

--- client/src/components/utils/utils.js
+++ client/src/components/utils/utils.js
@@ -1,16 +1,15 @@
 export const getTimeInZones = (zones, data) => {
-    let zonesArrays = {
-        
+	let zonesArrays = {
 		zone1: 0,
 		zone2: 0,
 		zone3: 0,
 		zone4: 0,
-        zone5: 0,
-        zone6: 0,
-        zone7: 0,
-    };
-    if (zones.name === 'heart_rate') {
-        data.forEach(dataPoint => {
+		zone5: 0,
+		zone6: 0,
+		zone7: 0,
+	};
+	if (zones.name === 'heart_rate') {
+		data.forEach(dataPoint => {
 			if (dataPoint < zones.z1) zonesArrays.zone1++;
 			if (dataPoint > zones.z1 && dataPoint < zones.z2)
 				zonesArrays.zone2++;
@@ -20,8 +19,8 @@ export const getTimeInZones = (zones, data) => {
 				zonesArrays.zone4++;
 			if (dataPoint > zones.z4) zonesArrays.zone5++;
 		});
-    }elseif (zones.name === 'power') {
-        data.forEach(dataPoint => {
+	} else if (zones.name === 'power') {
+		data.forEach(dataPoint => {
 			if (dataPoint < zones.z1) zonesArrays.zone1++;
 			if (dataPoint > zones.z1 && dataPoint < zones.z2)
 				zonesArrays.zone2++;
@@ -30,18 +29,16 @@ export const getTimeInZones = (zones, data) => {
 			if (dataPoint > zones.z3 && dataPoint < zones.z4)
 				zonesArrays.zone4++;
 			if (dataPoint > zones.z4 && dataPoint < zones.z5)
-                zonesArrays.zone4++;
-            if (dataPoint > zones.z5 && dataPoint < zones.z6)
-                zonesArrays.zone4++;
-            if (dataPoint > zones.z6)
 				zonesArrays.zone4++;
+			if (dataPoint > zones.z5 && dataPoint < zones.z6)
+				zonesArrays.zone4++;
+			if (dataPoint > zones.z6) zonesArrays.zone4++;
 		});
-    }
-	
+	}
 };
 
 const zones = {
-    name:'heart_rate',
+	name: 'heart_rate',
 	// maximum value
 	z1: 135,
 	z2: 150,
