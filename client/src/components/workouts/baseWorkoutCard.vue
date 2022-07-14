f0927b22a0eb3a1a045276ecb260bf8da51dced6
John Lockham <47853652+johnl6234@users.noreply.github.com>
2022-07-14 14:56:39 +0100

    edit workouts

--- client/src/components/workouts/baseWorkoutCard.vue
+++ client/src/components/workouts/baseWorkoutCard.vue
@@ -2,26 +2,61 @@
 	<div
 		class="mb-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md mx-auto"
 	>
-		<div :class="lap.type" class="px-4 py-1 text-slate-50 rounded-t-lg">
-			{{ lap.type }}
-		</div>
-		<div class="flex flex-row justify-evenly p-4">
-			<div class="px-6 flex-1 flex-col">
-				<div class="font-bold">{{ lap.durationType }}</div>
-				<div class="">{{ lap.duration }}</div>
+		<BaseWorkoutEdit v-if="isEditing" :lap="lap" @saveEdits="saveEdits" />
+		<div v-else>
+			<div
+				:class="lap.lapType"
+				class="px-4 py-1 text-slate-50 rounded-t-lg"
+			>
+				{{ lap.lapType }}
+				<span class="float-right text-black"
+					><button @click.prevent="$emit('deleteLap')">
+						X
+					</button></span
+				>
+				<span class="float-right mr-3 text-blue-300"
+					><button @click.prevent="toggleEditing">edit</button></span
+				>
 			</div>
-			<div class="px-6 flex-1 flex-col">
-				<div class="font-bold">{{ lap.targetType }}</div>
-				<div class="">{{ lap.target }}</div>
+			<div class="flex flex-row justify-evenly p-4">
+				<div class="px-6 flex-1 flex-col">
+					<div class="font-bold">
+						{{ lap.durationType.split('_').join(' ') }}
+					</div>
+					<div class="">{{ lap.duration }}</div>
+				</div>
+				<div class="px-6 flex-1 flex-col">
+					<div class="font-bold">
+						{{ lap.targetType.split('_').join(' ') }}
+					</div>
+					<div class="">{{ lap.target }}</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import BaseWorkoutEdit from './baseWorkoutEdit.vue';
 	export default {
 		name: 'workout-card',
 		props: ['lap'],
+		components: { BaseWorkoutEdit },
+		data() {
+			return {
+				isEditing: false,
+			};
+		},
+		methods: {
+			toggleEditing() {
+				this.isEditing = !this.isEditing;
+			},
+			saveEdits(data) {
+				this.toggleEditing();
+				console.log('data', data);
+				this.$emit('editLap', data);
+			},
+		},
 	};
 </script>
 <style scoped>
