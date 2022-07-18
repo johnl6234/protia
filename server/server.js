00d8f677f23f4d08632e492db51c79aa76e00ce5
John Lockham <47853652+johnl6234@users.noreply.github.com>
2022-07-18 10:16:14 +0100

    cors

--- server/server.js
+++ server/server.js
@@ -32,6 +32,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 
 app.use('/auth', verifyToken, TokenLogin);
 app.use('/register', registerRouter);
